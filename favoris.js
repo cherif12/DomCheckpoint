d2e35662a89ec89f9cf558c3e3a2b16a38d9bacb
cherif sassi <116743533+cherif12@users.noreply.github.com>
2023-03-31 11:55:53 +0100

    Add files via upload

---
+++ favoris.js
@@ -0,0 +1,60 @@
+function Toggle3(){
+    var btnvar = document.getElementById('btnh3')
+    if (btnvar.style.color == "red"){
+        btnvar.style.color = "grey"
+    }
+    else{
+        btnvar.style.color = "red"
+    }
+}
+
+function Toggle4(){
+    var btnvar = document.getElementById('btnh4')
+    if (btnvar.style.color == "red"){
+        btnvar.style.color = "grey"
+    }
+    else{
+        btnvar.style.color = "red"
+    }
+}
+
+function Toggle5(){
+    var btnvar = document.getElementById('btnh5')
+    if (btnvar.style.color == "red"){
+        btnvar.style.color = "grey"
+    }
+    else{
+        btnvar.style.color = "red"
+    }
+}
+
+function Toggle6(){
+    var btnvar = document.getElementById('btnh6')
+    if (btnvar.style.color == "red"){
+        btnvar.style.color = "grey"
+    }
+    else{
+        btnvar.style.color = "red"
+    }
+}
+
+function Toggle7(){
+    var btnvar = document.getElementById('btnh7')
+    if (btnvar.style.color == "red"){
+        btnvar.style.color = "grey"
+    }
+    else{
+        btnvar.style.color = "red"
+    }
+}
+
+function Toggle8(){
+    var btnvar = document.getElementById('btnh8')
+    if (btnvar.style.color == "red"){
+        btnvar.style.color = "grey"
+    }
+    else{
+        btnvar.style.color = "red"
+    }
+}
+
